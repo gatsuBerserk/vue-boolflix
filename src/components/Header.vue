a50f0b708f84b5ffc805c00450e94b974d6c7034
gatsuBerserk <berserkdev@outlook.it>
2022-04-01 12:09:12 +0200

    init

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      <h1>Header</h1>
+  </div>
+</template>
+
+<script>
+export default { 
+    name : "HeaderSearch",
+
+}
+</script>
+
+<style lang="scss" scoped> 
+@import "../assets/style/style.scss"
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,17 @@
+<template>
+  <div> 
+      <h1>Main</h1>
+  </div>
+</template>
+
+<script>
+export default { 
+    name : "MainPage",
+
+}
+</script>
+
+<style lang="scss" scoped> 
+@import "../assets/style/style.scss"
+
+</style>
